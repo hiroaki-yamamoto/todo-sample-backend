f5132c277c2a88eb0fd2a57911a43df2d9841823
Hiroaki Yamamoto <5356011+hiroaki-yamamoto@users.noreply.github.com>
2026-04-10 10:37:29 +0900

    fix: correct SQL syntax for dropping indexes in migration script

--- db/migrations/000001_init.down.sql
+++ db/migrations/000001_init.down.sql
@@ -1,5 +1,5 @@
-DROP INDEX IF EXISTS idx_todos_user_id ON todos;
-DROP INDEX IF EXISTS idx_todos_completed_at ON todos;
-DROP INDEX IF EXISTS idx_todos_wip_at ON todos;
-DROP TABLE IF EXISTS users;
+DROP INDEX IF EXISTS idx_todos_user_id;
+DROP INDEX IF EXISTS idx_todos_completed_at;
+DROP INDEX IF EXISTS idx_todos_wip_at;
 DROP TABLE IF EXISTS todos;
+DROP TABLE IF EXISTS users;
